8a05bff64c4e4fc791ea1dc1334f815f5dfa44d3
flaum <skflaum@gmail.com>
2016-08-22 10:03:01 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -60,4 +60,25 @@ $(document).ready(function() {
 
 	reverse(portfolioContainer);
 	adaptiveImgs(portfolioImgs, imgsUrlMob, imgsUrlTabl, imgsUrlDesk);
+
+	function posFix() {
+		var contHeight = parseInt($("#calculator").css("height").slice(0, $("#calculator").css("height").indexOf("px")));
+		var navHeight = parseInt($(".nav-price-wrap").css("height").slice(0, $(".nav-price-wrap").css("height").indexOf("px")));
+		var start = $("#calculator").offset().top;
+		var end = $("#advantages").offset().top - navHeight;
+		var width = $("#calculator").css("width");
+		var price = $(".nav-price-wrap").children(".total-price");
+
+		if($(window).scrollTop() + 104 >= start) {
+			$(".nav-price-wrap").css("position", "fixed").css("bottom", "0").css("left", "0").css("right", "0").css("z-index", "1000").css("width", width);
+			price.css("display", "flex").css("align-items", "center").css("padding", "0 20px").css("background-color", "#ededef").css("height", "64px").css("border-radius" , "50px");
+			if($(".nav-price-wrap").offset().top >= $("#advantages").offset().top - navHeight) {
+				$(".nav-price-wrap").css("position", "");
+				price.css("display", "").css("align-items", "").css("padding", "").css("background-color", "").css("height", "").css("border-radius" , "");
+			};
+		} else if($(window).scrollTop() < start) {
+			$(".nav-price-wrap").css("position", "");
+			price.css("display", "").css("align-items", "").css("padding", "").css("background-color", "").css("height", "").css("border-radius" , "");
+		};
+	};
 });
